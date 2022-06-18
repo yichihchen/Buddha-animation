e213785e697ca24705db0889d9afea0a3b43a23c
ingridchen <ingridiaim@gmail.com>
2022-06-18 10:56:41 +0800

    first commit

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,5 +1,50 @@
-console.log('Hello!');
+
+
+
+
 
 $(document).ready(() => {
   console.log('HesSchool Hello!');
+
+  $(".big-budaha-up").click(function () {
+    $(".big-budaha-up").hide()
+  })
+
+  $(".big-buddha-down").click(function () {
+    $(".big-budaha-up").show()
+  })
+
+  $(".hands-up").click(function () {
+    $(".hands-up").hide()
+  })
+
+  $(".hands-down").click(function () {
+    $(".hands-up").show()
+  })
+
+  $(".monks-up").click(function () {
+    $(".monks-up").hide()
+  })
+
+  $(".monks-down").click(function () {
+    $(".monks-up").show()
+  })
+  $(".moon-up").click(function () {
+    $(".moon-up").hide()
+  })
+
+  $(".moon-down").click(function () {
+    $(".moon-up").show()
+  })
+
+  $(".relic-up").click(function () {
+    $(".relic-up").hide()
+  })
+  $(".relic-down").click(function () {
+    $(".relic-up").show()
+  })
+
+
+
+
 });
--- gulpfile.js/index.js
+++ gulpfile.js/index.js
@@ -11,12 +11,12 @@ console.log(`Current mode：${options.env}`);
 
 function copyFile() {
   return gulp.src(envOptions.copyFile.src)
-  .pipe(gulp.dest(envOptions.copyFile.path))
-  .pipe(
-    browserSync.reload({
-      stream: true,
-    }),
-  );
+    .pipe(gulp.dest(envOptions.copyFile.path))
+    .pipe(
+      browserSync.reload({
+        stream: true,
+      }),
+    );
 }
 
 function layoutHTML() {
@@ -87,9 +87,9 @@ function browser() {
 
 function clean() {
   return gulp.src(envOptions.clean.src, {
-      read: false,
-      allowEmpty: true,
-    })
+    read: false,
+    allowEmpty: true,
+  })
     .pipe($.clean());
 }
 
@@ -113,3 +113,4 @@ exports.clean = clean;
 exports.build = gulp.series(clean, copyFile, layoutHTML, sass, babel, vendorsJs);
 
 exports.default = gulp.series(clean, copyFile, layoutHTML, sass, babel, vendorsJs, gulp.parallel(browser, watch));
+
