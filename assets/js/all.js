13841ad16f18fa47f926f76ca64a0b0832c4dd9c
ingridchen <ingridiaim@gmail.com>
2022-06-18 10:58:26 +0800

    Update 2022-06-18T02:57:45.260Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,7 +1,36 @@
 "use strict";
 
-console.log('Hello!');
 $(document).ready(function () {
   console.log('HesSchool Hello!');
+  $(".big-budaha-up").click(function () {
+    $(".big-budaha-up").hide();
+  });
+  $(".big-buddha-down").click(function () {
+    $(".big-budaha-up").show();
+  });
+  $(".hands-up").click(function () {
+    $(".hands-up").hide();
+  });
+  $(".hands-down").click(function () {
+    $(".hands-up").show();
+  });
+  $(".monks-up").click(function () {
+    $(".monks-up").hide();
+  });
+  $(".monks-down").click(function () {
+    $(".monks-up").show();
+  });
+  $(".moon-up").click(function () {
+    $(".moon-up").hide();
+  });
+  $(".moon-down").click(function () {
+    $(".moon-up").show();
+  });
+  $(".relic-up").click(function () {
+    $(".relic-up").hide();
+  });
+  $(".relic-down").click(function () {
+    $(".relic-up").show();
+  });
 });
 //# sourceMappingURL=all.js.map
